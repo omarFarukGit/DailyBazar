5ae953c726886833595628fbc58ec035d1e9e80b
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-30 18:37:31 +0600

    polish hero and feture section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className=" min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div className=" ">
       <Hero />
       <Fetures />
     </div>
--- client/src/components/home/Fetures.tsx
+++ client/src/components/home/Fetures.tsx
@@ -3,25 +3,27 @@ import React from "react";
 
 const Fetures = () => {
   return (
-    <section className=" bg-white py-5 border border-app-border/80 rounded-xl">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className=" grid grid-cols-2 md:grid-cols-4 gap-4">
-          {heroSectionData.hero_features.map((Fetures, i) => (
-            <div key={i} className="flex items-center gap-3 py-3">
-              <div className=" size-10 rounded-lg bg-app-cream flex justify-center items-center shrink-0">
-                <Fetures.icon className=" size-5" />
+    <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <section className=" bg-white py-5 border border-app-border/80 rounded-xl">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <div className=" grid grid-cols-2 md:grid-cols-4 gap-4">
+            {heroSectionData.hero_features.map((Fetures, i) => (
+              <div key={i} className="flex items-center gap-3 py-3">
+                <div className=" size-10 rounded-lg bg-app-cream flex justify-center items-center shrink-0">
+                  <Fetures.icon className=" size-5" />
+                </div>
+                <div>
+                  <p className=" text-sm font-semibold text-app-green">
+                    {Fetures.title}
+                  </p>
+                  <p className=" text-xs text-app-text-light">{Fetures.desc}</p>
+                </div>
               </div>
-              <div>
-                <p className=" text-sm font-semibold text-app-green">
-                  {Fetures.title}
-                </p>
-                <p className=" text-xs text-app-text-light">{Fetures.desc}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
