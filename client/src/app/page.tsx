c43700a1c1ca3caefd39889329490cbc700bdef7
omarFarukGit <muhammadabuhafz@gmail.com>
2026-05-30 18:33:43 +0600

    Added fetures sections

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,14 @@
+import Fetures from "@/components/home/Fetures";
 import Hero from "@/components/home/Hero";
 import React from "react";
 
 const Home = () => {
-  return <div>
-    <Hero/>
-  </div>;
+  return (
+    <div className=" min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <Hero />
+      <Fetures />
+    </div>
+  );
 };
 
 export default Home;
---
+++ client/src/components/home/Fetures.tsx
@@ -0,0 +1,28 @@
+import { heroSectionData } from "@/assets/assets";
+import React from "react";
+
+const Fetures = () => {
+  return (
+    <section className=" bg-white py-5 border border-app-border/80 rounded-xl">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className=" grid grid-cols-2 md:grid-cols-4 gap-4">
+          {heroSectionData.hero_features.map((Fetures, i) => (
+            <div key={i} className="flex items-center gap-3 py-3">
+              <div className=" size-10 rounded-lg bg-app-cream flex justify-center items-center shrink-0">
+                <Fetures.icon className=" size-5" />
+              </div>
+              <div>
+                <p className=" text-sm font-semibold text-app-green">
+                  {Fetures.title}
+                </p>
+                <p className=" text-xs text-app-text-light">{Fetures.desc}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Fetures;
